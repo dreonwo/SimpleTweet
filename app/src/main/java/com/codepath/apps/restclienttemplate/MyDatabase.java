aff92e7d40f2bddefe066e04a8edb1fe1354c077
dreonwo <dwheatleyowens@icloud.com>
2021-02-11 18:00:18 -0500

    Added Compose layout

--- app/src/main/java/com/codepath/apps/restclienttemplate/MyDatabase.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/MyDatabase.java
@@ -3,13 +3,12 @@ package com.codepath.apps.restclienttemplate;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-import com.codepath.apps.restclienttemplate.models.Organization;
 import com.codepath.apps.restclienttemplate.models.SampleModel;
 import com.codepath.apps.restclienttemplate.models.SampleModelDao;
 
-@Database(entities={Organization.User.class}, version=1)
+@Database(entities={SampleModel.class}, version=1)
 public abstract class MyDatabase extends RoomDatabase {
-    public abstract Organization.UserDao userDao();
+    public abstract SampleModelDao sampleModelDao();
 
     // Database name to be used
     public static final String NAME = "MyDataBase";
--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,12 +1,17 @@
 package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -28,12 +33,35 @@ public class TimelineActivity extends AppCompatActivity {
     TweetsAdapter adapter;
     private SwipeRefreshLayout swipeContainer;
     EndlessRecyclerViewScrollListener scrollListener;
+    Toolbar toolbar;
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.compose){
+            //Compose icon has been selected
+            //Navigate to the compose activity
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivity(intent);
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         client = TwitterApp.getRestClient(this);
 
@@ -60,8 +88,9 @@ public class TimelineActivity extends AppCompatActivity {
         rvTweets.addOnScrollListener(scrollListener);
         populateHomeTimeline();
 
-        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
 
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
         swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
